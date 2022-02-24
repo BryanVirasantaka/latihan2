6a9d4e7f85652bc3440d52581be2a6762f995f73
BryanVirasantaka <100253980+BryanVirasantaka@users.noreply.github.com>
2022-02-24 09:49:14 +0700

    Add files via upload

---
+++ Ayah.java
@@ -0,0 +1,13 @@
+
+package latian2;
+
+
+public class Ayah {
+    String Sifat= "penyabar";
+    double TB= 173.1;
+    
+    void hobbi(){
+        System.out.println("Hobi nya adalah Ngopiding = Ngopi dan Ngoding");
+    
+}
+}
---
+++ Hasil.java
@@ -0,0 +1,11 @@
+
+package latian2;
+
+public class Hasil {
+    public static void main(String[] args) {
+        Anak dina=new Anak();
+        dina.setnama("Dinaa");
+        dina.Cetak();
+    }
+    
+}
